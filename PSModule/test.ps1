389e3127e6775a7ead0a6df1eb13acf158a5e9f9
Thomas Kurth <thomas@famkurth.ch>
2020-04-26 13:41:12 +0200

    Test script start path independend

--- PSModule/test.ps1
+++ PSModule/test.ps1
@@ -1,5 +1,6 @@
-import-module .\IntuneDocumentation\IntuneDocumentation.psm1 -force
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+import-module "$scriptPath\IntuneDocumentation\IntuneDocumentation.psm1" -force
 Connect-MSGraph -ForceInteractive
 $rand = Get-Random -Minimum 0 -Maximum 100
 Invoke-IntuneDocumentation -FullDocumentationPath "C:\temp\docu$rand.docx" -UseTranslationBeta
-Write-Host "Created Documentation C:\temp\docu$rand.docx"
+Write-Host "Created Documentation C:\temp\docu$rand.docx"5
