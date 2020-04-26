3987400de58bfa722a2a06d8a18e81c11f09d718
Thomas Kurth <thomas@famkurth.ch>
2020-04-26 12:26:28 +0200

    Add Translation

--- PSModule/test.ps1
+++ PSModule/test.ps1
@@ -1,5 +1,5 @@
 import-module .\IntuneDocumentation\IntuneDocumentation.psm1 -force
 Connect-MSGraph -ForceInteractive
 $rand = Get-Random -Minimum 0 -Maximum 100
-Invoke-IntuneDocumentation -FullDocumentationPath "C:\temp\docu$rand.docx" 
+Invoke-IntuneDocumentation -FullDocumentationPath "C:\temp\docu$rand.docx" -UseTranslationBeta
 Write-Host "Created Documentation C:\temp\docu$rand.docx"
